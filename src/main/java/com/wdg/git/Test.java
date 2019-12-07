da6c169f7b5870767b2edcae405d37f42d7ddc5b
w467495697 <467495697@qq.com>
2019-12-07 09:44:25 +0800

    王杰

--- src/main/java/com/wdg/git/Test.java
+++ src/main/java/com/wdg/git/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
-
+        //测试
         System.out.println("第一次使用");
         System.out.println("第二次使用");
         /*
